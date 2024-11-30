78ab0e8de6924a3a0494ac8042b51ebb854d90e6
pzheng46 <peter@township.agency>
2024-11-30 16:04:26 -0500

    Deploy build to GitHub Pages

---
+++ precache-manifest.80effc9d18335f8146333c3a08728908.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7810be250ff9a3ae295a315c515cb7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da8f1a98088ef662e5c7",
+    "url": "/static/css/main.a4daf751.chunk.css"
+  },
+  {
+    "revision": "4c3625b2d44d7b7f516f",
+    "url": "/static/js/2.8f96facd.chunk.js"
+  },
+  {
+    "revision": "d3e6399bedfff23d2c3e2cdbe6af516e",
+    "url": "/static/js/2.8f96facd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da8f1a98088ef662e5c7",
+    "url": "/static/js/main.e4ba1ade.chunk.js"
+  },
+  {
+    "revision": "35f67668c217d61057f5",
+    "url": "/static/js/runtime-main.ed9d8f35.js"
+  },
+  {
+    "revision": "031523d97f38bd6ae35b83d0ad843b77",
+    "url": "/static/media/about.031523d9.png"
+  },
+  {
+    "revision": "e69e0682ea53bdae5c123c155e49916c",
+    "url": "/static/media/education.e69e0682.png"
+  },
+  {
+    "revision": "213a6a04cf2313f3bae6220e7aac3a61",
+    "url": "/static/media/github.213a6a04.png"
+  },
+  {
+    "revision": "71ef99928d816b46257bc90d3ebd58ba",
+    "url": "/static/media/info.71ef9992.png"
+  },
+  {
+    "revision": "3c29b9a4040347eb12e43b0d99771a77",
+    "url": "/static/media/insta.3c29b9a4.png"
+  },
+  {
+    "revision": "458afbb60b4d01932ec5801ccb597ee6",
+    "url": "/static/media/jp.458afbb6.png"
+  },
+  {
+    "revision": "7eca9ecb7e709c3a72a9779f075d5d0c",
+    "url": "/static/media/linkedin.7eca9ecb.png"
+  },
+  {
+    "revision": "bd454f8a773e3d82c6deb44369523c34",
+    "url": "/static/media/package.bd454f8a.png"
+  },
+  {
+    "revision": "064c9da7de145bb954ef9e5a0aced2d3",
+    "url": "/static/media/senior-life.064c9da7.png"
+  }
+]);
